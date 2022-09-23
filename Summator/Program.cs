cc73de449d0edc9d37e9fd9c0e48507762cdb2f2
uniyana <105173526+uniyana@users.noreply.github.com>
2022-09-23 18:14:58 +0300

    Add sumator files

---
+++ Summator/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace Summator 
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int result = Summator.Sum(new int[] {1, 1});
+            if (result == 2)
+            {
+                Console.WriteLine("Test PASS");
+            } else
+            {
+                Console.WriteLine("Test FAIL");
+            }
+
+        }
+    }
+}
+
---
+++ Summator/Summator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Summator
+{
+    public static class Summator
+    {
+        public static int Sum(int[] arr)
+        {
+            int sum = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                sum += arr[i];
+            }
+
+            return sum;
+        }
+    }
+}
